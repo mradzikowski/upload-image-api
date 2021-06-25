fac9b026c729532519f0d85d0bb7cd5f97f89b28
Mateusz Radzikowski <radzik.radzikowski18@gmail.com>
2021-06-25 21:34:35 +0200

    Add versatileimagefield to installed apps

--- app/drf_project/settings.py
+++ app/drf_project/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'versatileimagefield',
     'images'
 ]
 
@@ -127,3 +128,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'images.CustomUser'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
