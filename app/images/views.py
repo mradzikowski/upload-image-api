a14806460f8edbe9804471a5079cbb7ca97b4b75
Mateusz Radzikowski <radzik.radzikowski18@gmail.com>
2021-06-25 22:24:09 +0200

    Add views of accounts

--- app/images/views.py
+++ app/images/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.http import Http404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+from .models import Account
+from .serializers import AccountSerializer
 
 # Create your views here.
+
+
+class AccountList(APIView):
+    def post(self, request, format=None):
+        serializer = AccountSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def get(self, request, format=None):
+        accounts = Account.objects.all()
+        serializer = AccountSerializer(accounts, many=True)
+        return Response(serializer.data)
+
+
+class AccountDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Account.objects.get(pk=pk)
+        except Account.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        account = self.get_object(pk)
+        serializer = AccountSerializer(account)
+        return Response(serializer.data)
