27a5a995856b5eea7593f3c180bffba2e8ef2faf
Mateusz Radzikowski <radzik.radzikowski18@gmail.com>
2021-06-25 21:32:46 +0200

    Register models in admin.py

--- app/images/admin.py
+++ app/images/admin.py
@@ -1,3 +1,45 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as DefaultUserAdmin
+
+from .models import Account, CustomUser, User, Image
 
 # Register your models here.
+
+
+@admin.register(CustomUser)
+class UserAdmin(DefaultUserAdmin):
+    pass
+
+
+@admin.register(Account)
+class AccountAdmin(admin.ModelAdmin):
+    fields = (
+        "name",
+    )
+    list_display = (
+        "name",
+    )
+
+
+@admin.register(User)
+class _UserAdmin(admin.ModelAdmin):
+    fields = (
+        "username",
+        "account_type",
+    )
+    list_display = (
+        "username",
+        "account_type",
+    )
+
+
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    fields = (
+        "user_id",
+        "image",
+    )
+
+
+
+
