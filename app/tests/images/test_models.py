b9b6c404cb20a4573895717a14353d08b0b8a7f0
Mateusz Radzikowski <radzik.radzikowski18@gmail.com>
2021-06-25 21:34:48 +0200

    Add tests for models

---
+++ app/tests/images/test_models.py
@@ -0,0 +1,39 @@
+import pytest
+
+from images.models import Account, User, Image
+from versatileimagefield.fields import PPOIField
+
+
+@pytest.mark.django_db
+def test_account_model():
+    account = Account(name="Basic")
+    account.save()
+    assert account.name == "Basic"
+    assert str(account) == "Basic"
+
+
+@pytest.mark.django_db
+def test_user_model():
+    account = Account(name="Basic")
+    user = User(username="XYZ", account_type=account)
+    account.save()
+    user.save()
+    assert user.username == "XYZ"
+    assert str(user) == "XYZ"
+    assert user.account_type
+
+
+@pytest.mark.django_db
+def test_image_model():
+    account = Account(name="Basic")
+    account.save()
+    user = User(username="XYZ", account_type=account)
+    user.save()
+    image = Image(user_id=user)
+    image.save()
+
+    assert image.user_id == user
+    assert str(image) == str(user)
+
+
+
